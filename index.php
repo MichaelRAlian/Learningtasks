ac87ba3d67129117faaad9df3ae583a55c370547
MichaelRAlian <102036024+MichaelRAlian@users.noreply.github.com>
2022-03-22 10:06:21 +0800

    Update index.php

--- index.php
+++ index.php
@@ -4,10 +4,11 @@
         <title>My Home Page</title>
     </head>
     <body>
-   <h1>Welcome To My Website! I am Michael Alian!</hi>
+   <h1>Welcome To My Website! I am Michael Alian!</h1>
     <lu>
        <li><a href="index.php"HOME</a></li>
-       <li><a href=/LT2/homepage.html"><LEARNING TASK 2</a></li>
+       <li><a href="LT2/homepage.html"><Home</a></li>
+        <li><a href="LT2/Recipe.html"><Leaning task 2</a></li>
     </lu>
     </body>
 </html>
