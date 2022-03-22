128d951566742c8ab5edbb1b82fbce40eacc3eab
MichaelRAlian <102036024+MichaelRAlian@users.noreply.github.com>
2022-03-22 09:46:33 +0800

    Create index.php

---
+++ LT2/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>My Home Page</title>
+    </head>
+    <body>
+   <h1>Welcome To My Website! I am Michael Alian!</hi>
+    <lu>
+       <li><a href="index.php"HOME</a></li>
+       <li><a href=/LT2/index.html"><LEARNING TASK 2</a></li>
+    </lu>
+    </body>
+</html>
