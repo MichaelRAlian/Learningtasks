ccd1ce0a70286249edfd08ec5422a3439f03359c
MichaelRAlian <102036024+MichaelRAlian@users.noreply.github.com>
2022-03-22 10:13:34 +0800

    Update index.php

--- index.php
+++ index.php
@@ -7,8 +7,7 @@
    <h1>Welcome To My Website! I am Michael Alian!</h1>
     <ul>
        <li><a href="index.php">HOME</a></li>
-       <li><a href="LT2/homepage.html">Home</a></li>
-       <li><a href="LT2/Recipe.html">Leaning task 2</a></li>
+        <li><a href="LT2/homepage.html">Leaning task 2</a></li>
     </ul>
   </body>
 </html>
